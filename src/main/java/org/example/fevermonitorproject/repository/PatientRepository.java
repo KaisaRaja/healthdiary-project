8f665a02094790273addddf6cec9c9976d6d4e9b
Kaisa <raja.kaisa@gmail.com>
2024-12-04 14:41:55 +0200

    2

---
+++ src/main/java/org/example/fevermonitorproject/repository/PatientRepository.java
@@ -0,0 +1,8 @@
+package org.example.fevermonitorproject.repository;
+
+import org.example.fevermonitorproject.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {}
